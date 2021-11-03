806b16589a6ca4870928e7814a20514c573468f6
moses okocha <mosesokocha7@gmail.com>
2021-11-03 05:42:22 -0700

    check

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,7 @@ app.use('/api/products', productRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
@@ -40,9 +41,6 @@ app.get('*', (req, res) =>
 );
 
 
-// app.get('/', (req, res) => {
-//   res.send('Server is ready');
-// });
 
 
 
